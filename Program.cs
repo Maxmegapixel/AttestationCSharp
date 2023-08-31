5f366e907de4ab23099ce02999f27c29927f234c
Maksim <maxmegapixel@mail.ru>
2023-08-31 23:30:09 +0300

    task 2

--- Program.cs
+++ Program.cs
@@ -1,16 +1,20 @@
-﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
-// N = 5 -> "5, 4, 3, 2, 1"
-// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
 
+PrintMessage("Введите значение M:");
+int M = GetValue();
 PrintMessage("Введите значение N:");
-int num = int.Parse(Console.ReadLine()!);
-PrintMessage($"Натуральные числа в промежутке от {num} до 1:");
-PrintMessage(GetNumbers(num));
+int N = GetValue();
+PrintMessage($"Сумма в промежутке от {M} до {N}:");
+PrintMessage($"{GetSumm(M, N)}");
 
+int GetValue() => int.Parse(Console.ReadLine()!);
 void PrintMessage(string value) => Console.WriteLine(value);
-string GetNumbers(int value)
+
+int GetSumm(int start, int end)
 {
-    if (value == 1) return "1";
-    return (value + ", " + GetNumbers(value - 1)); 
+    if (start == end) return start;
+    return (start + GetSumm(start + 1, end)); 
 }
 
