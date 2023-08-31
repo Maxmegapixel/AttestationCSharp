44cd5d71c5a494c28593e181eb6bbf2d8d6ae830
Maksim <maxmegapixel@mail.ru>
2023-08-31 22:59:26 +0300

    task 1

--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-// M = 1; N = 15 -> 120
-// M = 4; N = 8. -> 30
+PrintMessage("Введите значение N:");
+int num = int.Parse(Console.ReadLine()!);
+PrintMessage($"Натуральные числа в промежутке от {num} до 1:");
+PrintMessage(GetNumbers(num));
 
-// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-// m = 2, n = 3 -> A(m,n) = 9
-// m = 3, n = 2 -> A(m,n) = 29
+void PrintMessage(string value) => Console.WriteLine(value);
+string GetNumbers(int value)
+{
+    if (value == 1) return "1";
+    return (value + ", " + GetNumbers(value - 1)); 
+}
 
